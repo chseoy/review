49bc72e6319c6d27eb910fe3e09f6385865a61c8
chseoy <dday8811@inha.edu>
2021-10-13 05:38:53 +0900

    v0.2 for문 반복 횟수 차감

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,20 +1,42 @@
 import java.util.Scanner;
-// v0.1
+//v0.2 for 반복 횟수 2회 감소
 public class ReviewDemo01 {
     public static void main(String[] args) {
-        // 소수 : 1과 자기자신 외에는 나누어 떨어지지 않는 수
+        // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int number = sc.nextInt();
         int cnt = 0;
 
-        for (int k = 1; k <= num; k++) {
-            if(num % k == 0)
+        for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
+            if(number % k == 0)
                 cnt++;
         }
 
-        if(cnt == 2)
-            System.out.println(num + "은(는) 소수입니다.");
+        if(cnt == 0) // 카운트가 0이 유지되면 true
+            System.out.println(number + "은(는) 소수!");
         else
-            System.out.println(num + "은(는) 소수가 아닙니다.");
+            System.out.println(number + "은(는) 소수가 아닙니다");
     }
 }
+
+
+//import java.util.Scanner;
+//// v0.1
+//public class ReviewDemo01 {
+//    public static void main(String[] args) {
+//        // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
+//        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        int cnt = 0;
+//
+//        for(int k=1; k<=number; k++){
+//            if(number % k == 0)
+//                cnt++;
+//        }
+//
+//        if(cnt == 2)
+//            System.out.println(number + "은(는) 소수!");
+//        else
+//            System.out.println(number + "은(는) 소수가 아닙니다");
+//    }
+//}
