4386586b9b2dd2b53eb14439d0fa19028df03979
chseoy <dday8811@inha.edu>
2021-10-13 05:42:40 +0900

    v0.3 boolean타입을 이용하여 가독성 증가

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
-//v0.2 for 반복 횟수 2회 감소
+//v0.3 가독성 증가
 public class ReviewDemo01 {
     public static void main(String[] args) {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int cnt = 0;
+        boolean isPrime = true;
 
         for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
             if(number % k == 0)
-                cnt++;
+                isPrime = false;
         }
 
-        if(cnt == 0) // 카운트가 0이 유지되면 true
+        if(isPrime) // isPrime 변수 값이 true면
             System.out.println(number + "은(는) 소수!");
         else
             System.out.println(number + "은(는) 소수가 아닙니다");
     }
 }
 
-
 //import java.util.Scanner;
-//// v0.1
+////v0.2 for 반복 횟수 2회 감소
 //public class ReviewDemo01 {
 //    public static void main(String[] args) {
 //        // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -29,14 +28,36 @@ public class ReviewDemo01 {
 //        int number = sc.nextInt();
 //        int cnt = 0;
 //
-//        for(int k=1; k<=number; k++){
+//        for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
 //            if(number % k == 0)
 //                cnt++;
 //        }
 //
-//        if(cnt == 2)
+//        if(cnt == 0) // 카운트가 0이 유지되면 true
 //            System.out.println(number + "은(는) 소수!");
 //        else
 //            System.out.println(number + "은(는) 소수가 아닙니다");
 //    }
 //}
+//
+//
+////import java.util.Scanner;
+////// v0.1
+////public class ReviewDemo01 {
+////    public static void main(String[] args) {
+////        // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
+////        Scanner sc = new Scanner(System.in);
+////        int number = sc.nextInt();
+////        int cnt = 0;
+////
+////        for(int k=1; k<=number; k++){
+////            if(number % k == 0)
+////                cnt++;
+////        }
+////
+////        if(cnt == 2)
+////            System.out.println(number + "은(는) 소수!");
+////        else
+////            System.out.println(number + "은(는) 소수가 아닙니다");
+////    }
+////}
