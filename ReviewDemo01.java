f3531a4732681407b817551a8e58ba0bbefca737
chseoy <dday8811@inha.edu>
2021-10-13 05:51:06 +0900

    v0.6 for문을 while문으로 변경

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//v0.5 for문 안의 불필요한 반복횟수 더더더 줄이기
+//v0.6 for문을 while문으로
 public class ReviewDemo01 {
     public static void main(String[] args) {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -7,11 +7,13 @@ public class ReviewDemo01 {
         int number = sc.nextInt();
         boolean isPrime = true;
 
-        for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
-            if(number % k == 0) {
+        int k = 2;
+        while(k<number) {
+            if (number % k == 00) {
                 isPrime = false;
-                break; // 첫번째 약수가 발견되면 for문 탈출
+                break;
             }
+            k++;
         }
 
         if(isPrime) // isPrime 변수 값이 true면
@@ -20,6 +22,29 @@ public class ReviewDemo01 {
             System.out.println(number + "은(는) 소수가 아닙니다");
     }
 }
+
+//import java.util.Scanner;
+////v0.5 for문 안의 불필요한 반복횟수 더더더 줄이기
+//public class ReviewDemo01 {
+//    public static void main(String[] args) {
+//        // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
+//        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        boolean isPrime = true;
+//
+//        for(int k=2; k<number; k++){ // 반복 횟수 2회 감소
+//            if(number % k == 0) {
+//                isPrime = false;
+//                break; // 첫번째 약수가 발견되면 for문 탈출
+//            }
+//        }
+//
+//        if(isPrime) // isPrime 변수 값이 true면
+//            System.out.println(number + "은(는) 소수!");
+//        else
+//            System.out.println(number + "은(는) 소수가 아닙니다");
+//    }
+//}
 
 //import java.util.Scanner;
 ////v0.3 가독성 증가
